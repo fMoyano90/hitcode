ce6893d66147f0caf0aac74ffdfda2a99ef8c79b
Felipe <f.moyano90@gmail.com>
2020-04-16 21:31:53 -0400

    Versión en desarrollo

--- src/app/components/blog/blog.component.ts
+++ src/app/components/blog/blog.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import * as AOS from "aos";
+declare var particlesJS: any;
 
 @Component({
-  selector: 'app-blog',
-  templateUrl: './blog.component.html',
-  styleUrls: ['./blog.component.scss']
+  selector: "app-blog",
+  templateUrl: "./blog.component.html",
+  styleUrls: ["./blog.component.scss"],
 })
 export class BlogComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
-  }
+    AOS.init();
 
+    particlesJS.load(
+      "particles-js",
+      "assets/json/particlesjs-config.json",
+      function () {
+        console.log("callback - particles.js config loaded");
+      }
+    );
+  }
 }
